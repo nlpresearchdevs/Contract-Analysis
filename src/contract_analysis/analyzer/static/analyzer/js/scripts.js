36901ac4bf0fe3d5c939173405ec20be537fda26
Christian Mante <cmante@csyoutsourcing.com>
2019-01-18 16:41:30 +0800

    filter pane disable other checkboxes

--- src/contract_analysis/analyzer/static/analyzer/js/scripts.js
+++ src/contract_analysis/analyzer/static/analyzer/js/scripts.js
@@ -7,6 +7,16 @@ $(function () {
     });
 });
 
+$(function () {
+    $("[class='suggestions']").popover({
+        html: true,
+        content: function() {
+            console.log('click');
+            return $(this).next("#suggestions-popover-content").html();
+        }
+    });
+});
+
 $(document).on('click', function (e) {
     $('[data-toggle="popover"]').each(function () {
         //the 'is' for buttons that trigger popups
@@ -14,7 +24,6 @@ $(document).on('click', function (e) {
         if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
             (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
         }
-
     });
 });
 
@@ -29,36 +38,97 @@ window.onload = function() {
 };
 
 $(function () {
-    $('input[type=checkbox]').on('click', function(e) {
+    $('.filterPaneCheckBox').on('click', function(e) {
         var checkBoxId = $(this).attr('id');
-        var categoryText = $('.click[data-categoryList*="' + checkBoxId + '"], .click[data-naturePartyList*="' + checkBoxId + '"]');
-        if(categoryText.length) {
-            if ($(this).is(':checked')) {
-                // console.log('has ' + checkBoxId );
-                // console.log(categoryText.data());
-                categoryText.first($(this).focus());
-                categoryText.each(function(){
-                    // $(this).addClass("focus");
-                    // $(this).focus();
-                    $(this).css({"background": "#9ad7ff"});
-                    // console.log(this);
-                    $(this).popover({
-                        html: true,
-                        content: function() {
-                            return $(this).next("#popover-content").html();
-                        }
-                    });
-                })
-            }
-            else {
-                categoryText.each(function(){
-                    $(this).css({"background": ""});
-                })
-            }
-        } else {
-            console.log( 'Does not have ' + checkBoxId );
-            // console.log(categoryText.data());
-        }     
+        // get all contract text elements where categoryList and naturePartyList contains checkboxID
+        var contractText = $('.click[data-categoryList*="' + checkBoxId + '"], .click[data-naturePartyList*="' + checkBoxId + '"]');
+        var categoryArr = new Array();
+
+        contractText.each(function() {
+            var categoryList = $(this).attr('data-categoryList');
+            categoryList = categoryList.replace(/[\[\]']+/g,'').split(",");
+            categoryArr =  categoryArr.concat(categoryList);
+            // console.log(categoryArr);
+        });
+
+        categorySet = [...new Set(categoryArr)];
+
+        // var categoryArrayFlat = categoryList.join().split(",");
+        // var categorySet = new Set(categoryArr);
+        // console.log("categorySet: " + [...categorySet].join(' '));
+        console.log(categorySet);
+        console.log("includes: " + categorySet.includes('Assignments'));
+
+        // var categoryArr = new Array();
+        // var disableCheckBox = true;
+        // for(var text in contractText){
+        //     var categoryList = text.attr('data-categoryList');
+             
+        //     var categorySet = categoryList.join().split(",");
+            
+        //     if(!categorySet.includes(category)) {
+        //         categoryArr.push(category);
+        //     }
+        // }
+
+        //  = !(categoryArr.includes(checkBoxId));
+
+        // console.log('checkBoxID is not in the list: ' + disableCheckBox);
         
+        var checkBoxClass = $(this).attr('class');
+        var otherCheckBoxes = '';
+
+        var otherCategories = $('.categoryCheckBox').not(this).not('.click[data-categoryList*="' + categorySet + '"]');
+        var otherNatures = $('.natureCheckBox').not(this);
+        var otherParties = $('.partyCheckBox').not(this);
+
+        if(~checkBoxClass.indexOf('categoryCheckBox')) {
+            otherCheckBoxes = otherCategories
+        } else if(~checkBoxClass.indexOf('natureCheckBox')) {
+            otherCheckBoxes = otherNatures
+        } else {
+            otherCheckBoxes = otherParties
+        }
+
+        // console.log('otherCheckBoxes: ' + otherCheckBoxes.data());
+        // console.log('checkBoxClass: ' + checkBoxClass);
+
+        // this = selected checkbox
+        if ($(this).is(':checked')) {
+            contractText.first($(this).focus());
+            contractText.each(function(){
+                // this = current contractText
+                $(this).css({"background": "#9ad7ff"});
+                $(this).popover({
+                    html: true,
+                    content: function() {
+                        return $(this).next("#popover-content").html();
+                    }
+                });
+            });
+
+            otherCheckBoxes.each(function(){
+                // this = all except selected checkbox
+                if($(this).is(':checked')) {
+                    $(this).prop('checked', false);
+                } 
+                $(this).prop('disabled', true);
+                
+            });
+            
+        } else {
+            otherCheckBoxes.each(function(){
+                // this = all except selected checkbox
+                if($(this).is(':disabled')) {
+                    $(this).prop('disabled', false);
+                } 
+            });
+            contractText.each(function(){
+                // this = current contractText
+                $(this).css({"background": ""});
+            })
+        }
+
+
     });
 });
